8e7f2389876fbbb63ddbcc0d4ed085fcd8f7317f
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-04-28 21:27:59 +0200

    Update nav.js

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -3,8 +3,8 @@
     { href: 'commandes.html', label: 'Commandes' },
     { href: 'reiatsu.html',   label: 'Reiatsu' },
     { href: 'guesser.html',   label: 'Character Guesser' },
-    { href: 'chapitre.html',   label: '????' },
-    { href: 'scanguess.html',   label: 'Installation' },
+    { href: 'scanguess.html',   label: '????' },
+    { href: 'install.html',   label: 'Installation' },
   ];
   const themes = [
     { id: 'shinigami',    label: 'Shinigami',    icon: '⚔️' },
